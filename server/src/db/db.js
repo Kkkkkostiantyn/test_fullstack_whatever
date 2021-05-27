9d435984e9bff668152b3febb6aeea5f5e8535ca
Kostiantyn <zbryskyi.kostiantyn@gmail.com>
2021-05-27 14:49:17 +0300

    code smell

--- server/src/db/db.js
+++ server/src/db/db.js
@@ -1,14 +1,3 @@
-// const Pool = require("pg").Pool;
-// const pool = new Pool({
-//   user: "postgres",
-//   password: "admin",
-//   host: "localhost",
-//   port: 5432,
-//   database: "fullstack_whatever",
-// });
-//
-// module.exports = pool;
-
 const Sequelize = require("sequelize");
 
 const sequelize = new Sequelize(
